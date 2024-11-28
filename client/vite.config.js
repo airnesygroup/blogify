1cb3dbc87f7fcfaca8b2e4f51d5478241daa7ba0
airnesygroup <airnesy.info@gmail.com>
2024-11-28 20:50:55 +0300

    second  commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Vite config
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Use "/" if deploying to the root of your domain, adjust if deploying to a subdirectory
-  build: {
-    outDir: 'build', // Or 'dist', depending on your setup
-    rollupOptions: {
-      input: '/src/main.jsx', // Make sure the entry file is correctly set
-    },
-  },
 })
